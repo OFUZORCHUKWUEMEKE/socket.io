c56873bdfcf5281015dfb08e89d814de4e9d4afe
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2021-04-13 06:01:03 +0100

    first deploy

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+
+const http = require('http')
+
+const socketio = require('socket.io')
+
+const PORT = 6000
+
+const app = express()
+
+const {getUser,removeUser , getUsersInRoom,addUser} = require('./users')
+
+const cors = require('cors')
+
+const router = require('./router')
+
+const server = http.createServer(app)
+
+
+
+const io = socketio(server,{
+ cors:{
+    origin:'*',
+    method:["GET","POST"],
+    // Access-Control-Allow-Origin:'',
+
+    allowedHeaders:["Content-Type","Authorization"],
+    Credentials:true
+ }
+    
+})
+app.use(router) 
+app.use(cors())
+
+
+io.on('connect', (socket) => { 
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+    
+        if(error) return callback(error);   
+    
+        socket.join(user.room);
+    
+        socket.emit('message', { user: 'admin', text: `${user.name}, WELCOME TO ROOM ${user.room}.`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+    
+        callback();
+      });
+   
+    socket.on('sendMessage',(message,callback) =>{ 
+        const user = getUser(socket.id)
+        console.log(message);
+        if(!user){
+          console.log('errors');
+          // socket.off()
+        }
+        else{
+          io.to(user.room).emit('message',{user:user.name,text:message})
+          console.log(user); 
+          callback()
+        }
+       
+       
+        
+
+    })
+    // socket.on('typing',({name,room})=>{
+    //   const user = getUser(socket.id)
+    //   socket.to(user.room).broadcast.emit('typing',{user:user.name,text:`${user.name} is typing ..`})
+    // }) 
+
+    socket.on('disconnect',()=>{
+    const user = removeUser(socket.id);  
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
+}) 
+
+
+
+
+server.listen(5000,()=>{
+    console.log('app listening at port 5000')
+})
+
